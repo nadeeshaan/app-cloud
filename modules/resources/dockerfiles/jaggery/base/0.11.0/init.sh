c96d0fda53e3adb9bcddfb794bb95eb30744fa9e
Lahiru Manohara <lmanohara99@gmail.com>
2016-05-09 16:52:57 +0530

    Fixing carbon home redirection in jaggery apptype

--- modules/resources/dockerfiles/jaggery/base/0.11.0/init.sh
+++ modules/resources/dockerfiles/jaggery/base/0.11.0/init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 #remove default java opts
-sed -i '/-Xms256m/d' /opt/wso2as-5.3.0/bin/wso2server.sh 
+sed -i '/-Xms256m/d' /opt/wso2as-5.3.0/bin/wso2server.sh
+
+sed -i "/port=\"9763\"/a  \\\t\t   proxyPort=\"80\"" /opt/wso2as-5.3.0/repository/conf/tomcat/catalina-server.xml
+sed -i "/port=\"9443\"/a  \\\t\t   proxyPort=\"443\"" /opt/wso2as-5.3.0/repository/conf/tomcat/catalina-server.xml
 
 /opt/wso2as-5.3.0/bin/wso2server.sh -Dprofile=jaggery
