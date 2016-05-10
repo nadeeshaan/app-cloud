9288bdaa9a7c823edbc7fc9d665dd7322162fa51
mahesh <mahesh@wso2.com>
2016-05-10 17:37:25 +0530

    Changing admin password of wso2as inside user_mgt for jaggery apps Adding modules and patches for wso2as for jaggery apps

--- modules/resources/dockerfiles/jaggery/base/0.11.0/init.sh
+++ modules/resources/dockerfiles/jaggery/base/0.11.0/init.sh
@@ -6,4 +6,21 @@ sed -i '/-Xms256m/d' /opt/wso2as-5.3.0/bin/wso2server.sh
 sed -i "/port=\"9763\"/a  \\\t\t   proxyPort=\"80\"" /opt/wso2as-5.3.0/repository/conf/tomcat/catalina-server.xml
 sed -i "/port=\"9443\"/a  \\\t\t   proxyPort=\"443\"" /opt/wso2as-5.3.0/repository/conf/tomcat/catalina-server.xml
 
+#Changing admin password
+if [ -z ${ADMIN_PASSWORD+x} ]; then
+    echo "ADMIN_PASSWORD is not set.";
+    echo "Genarating admin password.";
+    ADMIN_PASSWORD=${ADMIN_PASS:-$(pwgen -s 12 1)}
+    echo "========================================================================="
+    echo "Credentials for the instance:"
+    echo
+    echo "    user name: admin"
+    echo "    password : $ADMIN_PASSWORD"
+    echo "========================================================================="
+    sed -i "s/.*<Password>admin<\/Password>.*/<Password>$ADMIN_PASSWORD<\/Password>/" /opt/wso2as-5.3.0/repository/conf/user-mgt.xml
+else
+    echo "ADMIN_PASSWORD set by user.";
+fi
+
+
 /opt/wso2as-5.3.0/bin/wso2server.sh -Dprofile=jaggery
