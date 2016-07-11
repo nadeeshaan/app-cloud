00ddad683ee0b0e67c11ee655b74f937909d28fa
mahesh <mahesh@wso2.com>
2016-07-11 14:26:00 +0530

    adding left menu to a seperate template

--- modules/jaggeryapps/appmgt/src/site/themes/default/templates/runtimeLogs/js/runtimeLogs.js
+++ modules/jaggeryapps/appmgt/src/site/themes/default/templates/runtimeLogs/js/runtimeLogs.js
@@ -246,17 +246,3 @@ function saveTextAsFile(textToWrite) {
     $("#log_download_progress_modal").hide();
     downloadLink.click();
 }
-
-$('.side-pane-trigger').click(function(){
-    var rightPane = $('.right-pane');
-    var leftPane = $('.left-pane');
-    if (rightPane.hasClass('visible')){
-        rightPane.animate({"left":"0em"}, "slow").removeClass('visible');
-        leftPane.animate({"left":"-18em"}, "slow");
-        $(this).find('i').removeClass('fa-arrow-left').addClass('fa-reorder');
-    } else {
-        rightPane.animate({"left":"18em"}, "slow").addClass('visible');
-        leftPane.animate({"left":"0em"}, "slow");
-        $(this).find('i').removeClass('fa-reorder').addClass('fa-arrow-left');
-    }
-});
