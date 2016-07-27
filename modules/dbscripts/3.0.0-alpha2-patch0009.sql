5f9d266866c864ac30c3211d8122df2b5aea3c9b
danushkf <danushkaf@wso2.com>
2016-07-27 09:07:52 +0530

    Fixing APPCLOUD-223

--- modules/dbscripts/3.0.0-alpha2-patch0009.sql
+++ modules/dbscripts/3.0.0-alpha2-patch0009.sql
@@ -1,3 +1,20 @@
+--
+--  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+--
+--    WSO2 Inc. licenses this file to you under the Apache License,
+--    Version 2.0 (the "License"); you may not use this file except
+--    in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing,
+--    software distributed under the License is distributed on an
+--    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+--    KIND, either express or implied.  See the License for the
+--    specific language governing permissions and limitations
+--    under the License.
+--
 -- to make identical local setup database with production database
 delete from AC_RUNTIME_CONTAINER_SPECIFICATIONS where id = 2 and CON_SPEC_ID = 1;
 insert into AC_RUNTIME_CONTAINER_SPECIFICATIONS values(5,4);
--- modules/setup-scripts/conf/wso2das-3.0.1/repository/deployment/server/jaggeryapps/monitoring/gadgets/stacked-line-chart/js/flot/jquery.flot.axislabels.js
+++ modules/setup-scripts/conf/wso2das-3.0.1/repository/deployment/server/jaggeryapps/monitoring/gadgets/stacked-line-chart/js/flot/jquery.flot.axislabels.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 (function($){var options={};function init(plot){var secondPass=false;var defaultPadding=2;plot.hooks.draw.push(function(plot,ctx){if(!secondPass){$.each(plot.getAxes(),function(axisName,axis){var opts=axis.options||plot.getOptions()[axisName];if(!opts||!opts.axisLabel)
 return;var w,h;var padding=opts.axisLabelPadding===undefined?defaultPadding:opts.axisLabelPadding;if(opts.axisLabelUseCanvas!=false)
 opts.axisLabelUseCanvas=true;if(opts.axisLabelUseCanvas){if(!opts.axisLabelFontSizePixels)
