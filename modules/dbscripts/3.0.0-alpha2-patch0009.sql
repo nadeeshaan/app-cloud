eba9c3083b6af82175fac0ea9b1aa257c49a5785
Amalka Subasinghe <amalkasubasinghe@gmail.com>
2016-07-27 09:23:13 +0530

    adding integration cloud perspective and db script update

--- modules/dbscripts/3.0.0-alpha2-patch0009.sql
+++ modules/dbscripts/3.0.0-alpha2-patch0009.sql
@@ -1,9 +1,13 @@
+-------------------------------------------------------------------------------------------------
 -- to make identical local setup database with production database
+-------------------------------------------------------------------------------------------------
 delete from AC_RUNTIME_CONTAINER_SPECIFICATIONS where id = 2 and CON_SPEC_ID = 1;
 insert into AC_RUNTIME_CONTAINER_SPECIFICATIONS values(5,4);
 insert into AC_RUNTIME_CONTAINER_SPECIFICATIONS values(6,4);
 
+-------------------------------------------------------------------------------------------------
 -- app types per cloud
+-------------------------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `AppCloudDB`.`AC_CLOUD` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
@@ -36,3 +40,28 @@ INSERT INTO `AC_CLOUD_APP_TYPE` (`cloud_id`, `app_type_id`) VALUES
 (1, 4),
 (1, 5);
 
+-------------------------------------------------------------------------------------------------
+-- adding esb app type
+-------------------------------------------------------------------------------------------------
+INSERT INTO `AC_APP_TYPE` (`id`, `name`, `description`) VALUES
+(6, 'car', 'Allows you to deploy a esb configuration that is supported in WSO2 Enterprise Service Bus');
+
+INSERT INTO `AC_RUNTIME` (`id`, `name`, `repo_url`, `image_name`, `tag`, `description`) VALUES
+(9, 'WSO2 Enterprise Service Bus - 5.0.0','registry.docker.appfactory.private.wso2.com:5000', 'wso2esb', '5.0.0', 'OS:Debian, Java Version:7u101');
+
+INSERT INTO `AC_APP_TYPE_RUNTIME` (`app_type_id`, `runtime_id`) VALUES
+(6, 9);
+
+INSERT INTO `AC_RUNTIME_CONTAINER_SPECIFICATIONS` (`id`, `CON_SPEC_ID`) VALUES
+(9, 3),
+(9, 4);
+
+-------------------------------------------------------------------------------------------------
+-- adding integration cloud
+-------------------------------------------------------------------------------------------------
+
+INSERT INTO `AC_CLOUD` (`id`, `name`) VALUES
+(2, 'integration-cloud');
+
+INSERT INTO `AC_CLOUD_APP_TYPE` (`cloud_id`, `app_type_id`) VALUES
+(2, 6);
